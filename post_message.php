07b213894416eea2be7df6e62a6a1849f5f419d7
Jakub Nedorost <jakub.nedorost@mallgroup.com>
2020-05-07 22:26:24 +0200

    a

---
+++ post_message.php
@@ -0,0 +1,13 @@
+<?php 
+$con = mysqli_connect("c072um.forpsi.com","f132381","26D7thmq","f132381");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Nebylo možné se připojit k databázi MySQL: " . mysqli_connect_error();
+  }
+if(isset($_POST['message']) && !isset($_POST['email'])) {
+  //pokud se postuje email, je to nejspíše vyplněné botem
+    $message = mysqli_real_escape_string($con,$_POST['message']);
+    $ins_query="insert into ripomer (`message`) values ('$message')";
+    mysqli_query($con,$ins_query) or die(mysqli_error($con));
+}    
